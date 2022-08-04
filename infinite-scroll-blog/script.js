fd2df5c6371cc9abacb2e0dd7b616d4bc37f7ca1
Eduard Efremov <ed.efremoff@outlook.com>
2022-08-04 11:54:29 +0300

    Ver 1.0

---
+++ infinite-scroll-blog/script.js
@@ -0,0 +1,17 @@
+const postsContainer = document.getElementById('post-container'),
+  loading = document.querySelector('.loader'),
+  filter = document.getElementById('filter');
+
+let limit = 3;
+let page = 1;
+
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json()
+  return data;
+
+}
+
