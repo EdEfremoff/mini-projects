395a724dbf182e3de2015832057e2670bd834983
Eduard Efremov <ed.efremoff@outlook.com>
2022-08-16 11:33:59 +0300

    Some bugs fix

--- lyrics-search-app/script.js
+++ lyrics-search-app/script.js
@@ -6,13 +6,6 @@ const more = document.getElementById('more');
 const apiURL = 'https://api.lyrics.ovh';
 
 // Search by song or artist
-
-/* const searchSongs = term => {
-  fetch(`${apiURL}/suggest/${term}`)
-    .then(res => res.json())
-    .then(data => console.log(data));
-}; */
-
 async function searchSongs(term) {
   const res = await fetch(`${apiURL}/suggest/${term}`);
   const data = await res.json();
@@ -22,24 +15,6 @@ async function searchSongs(term) {
 
 // Show data
 function showData(data) {
-  /* let output = '';
-
-  data.data.forEach(song => {
-    output += `
-        <li>
-            <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-            <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">
-                Get Lyrics
-            </button>
-        </li>
-        `;
-  });
-
-  result.innerHTML = `
-    <ul class="songs">
-      ${output}
-    </lu>
-  ` */
   result.innerHTML = `
   <ul class="songs">
   ${data.data
